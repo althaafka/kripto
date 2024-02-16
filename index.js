7247de4aa3dc2a20ba0373e51a1fb1c24f68bdcc
althaafka <althaafka004@gmail.com>
2024-02-16 22:21:09 +0700

    feat show answer

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ const sendData = (res, data) => {
 app.post("/encrypt/:alg", function (req, res) {
     const alg = req.params.alg;
 
+    let key = req.body.key.replace(/\s/g,'') ;
+    let input = req.body.input.replace(/\s/g,'') ;
+
     if (alg == "vigenere") {
         sendData(res, "vigenere");
     } else if (alg == "playfair") {
