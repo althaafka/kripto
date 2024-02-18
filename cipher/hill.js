f453582edfaac025702ef262ba5431d43ac4ffbe
althaafka <althaafka004@gmail.com>
2024-02-18 21:55:12 +0700

    feat hill cipher algo

---
+++ cipher/hill.js
@@ -0,0 +1,70 @@
+const math = require('mathjs');
+
+module.exports = {
+    encrypt: (key, input) => {
+        return encrypt(m, b, input);
+    },
+
+    decrypt: (key, input) => {
+        return decrypt(m, b, input);
+    }
+}
+
+const encrypt = (key, input) => {
+    let keyMatrix = strToMatrix(key, true)
+    let inputMatrix = strToMatrix(input, false)
+
+    let result = []
+
+    for (let i=0; i<inputMatrix.length; i++){
+        let temp = []
+        inputMatrix[i].map(x => temp.push(x))
+        math.multiply(keyMatrix, temp).map(
+            x => result.push(String.fromCharCode(x % 26 + 97))
+        )
+    }
+
+    return result.toString().replaceAll(',','');
+}
+
+const decrypt = (key, input) => {
+    let keyMatrix = strToMatrix(key, true)
+    let inputMatrix = strToMatrix(input, false)
+
+    let detK = math.det(keyMatrix)
+    let kInverse = math.round(math.multiply(detK, math.inv(keyMatrix)))
+    detK = math.invmod(detK % 26, 26)
+    kInverse = math.multiply(math.mod(kInverse, 26), detK)
+
+    let result = []
+
+    for (let i=0; i<inputMatrix.length; i++){
+        let temp = []
+        inputMatrix[i].map(x => temp.push(x))
+        math.multiply(kInverse, temp).map(
+            x => result.push(String.fromCharCode(x % 26 + 97))
+        )
+    }
+
+    return result.toString().replaceAll(',','');
+}
+
+const strToMatrix = (str, isKey) =>{
+    while (str.length % 3 != 0) str+="x"
+    let result = []
+    const iMax = isKey? 3 : (str.length/3);
+    for (let i=0; i<iMax; i++){
+        let row = []
+        for (let j=0; j<3; j++){
+            row.push(str.charCodeAt(i*3 + j) - 97)
+        }
+        result.push(row)
+    }
+    return result
+}
+
+
+console.log(encrypt("rrfvsvcct", "paymoremoney"))
+console.log(decrypt("rrfvsvcct", "lnshdlewmtrw"))
+console.log(encrypt("gybnqkurp", "act"))
+console.log(decrypt("gybnqkurp", "poh"))
---
+++ cipher/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+    // for (let i=0; i<inputMatrix.length; i++){
+    //     result.push(math.multiply(keyMatrix, inputMatrix))
+    // }
